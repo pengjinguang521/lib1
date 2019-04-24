12d3ddb0cc85aea962279d8a0a118b2990b21232
彭金光 <745758483@qq.com>
2019-04-24 13:57:27 +0800

    第一次提交代码

---
+++ lib1/lib1.h
@@ -0,0 +1,16 @@
+//
+//  lib1.h
+//  lib1
+//
+//  Created by iosgroup on 2019/4/24.
+//  Copyright © 2019 iosgroup. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface lib1 : NSObject
+
+
+- (void)testlib1;
+
+@end
---
+++ lib1/lib1.m
@@ -0,0 +1,17 @@
+//
+//  lib1.m
+//  lib1
+//
+//  Created by iosgroup on 2019/4/24.
+//  Copyright © 2019 iosgroup. All rights reserved.
+//
+
+#import "lib1.h"
+
+@implementation lib1
+
+- (void)testlib1 {
+    NSLog(@"testlib1");
+}
+
+@end
